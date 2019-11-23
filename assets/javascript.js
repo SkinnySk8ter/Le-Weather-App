0b28dc4f6e4c8c825f6c8671b7a4b5efbc32af71
Jody Jones <jodybjones@yahoo.com>
2019-11-23 11:46:23 -0500

    working on getting api calls to produce data into html document

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,25 @@
-var APIKey = "17bc6b3b7f3e498ad7423208cfc28809";
+var APIKey = "17bc6b3b7f3e498ad7423208cfc28809"
 
-var queryurl = "api.openweathermap.org/data/2.5/forecast?q={city},{country}" + APIKey
+var currenturl = "http://api.openweathermap.org/data/2.5/weather?q={city name}" + APIKey
+var fiveday = "http://api.openweathermap.org/data/2.5/forecast?q={city name},{country code}" + APIKey
+var uvindex = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}" + APIKey
 
 //need to make the button get the data on the click!! That goes RIGHT HERE BUDDY
 
 
+
 $.ajax({
-    url: queryURL,
+    url: currenturl,
     method: "GET"
   }).then(function(response) {
+    console.log(currenturl);
+    console.log(response);
 
-    $(".city").html("<h1>" + response.name + "Weather Details</h1>");
+    $("#theArea").html("<h1>" + response.name + "Weather Details</h1>");
+  })
+
+  $("btn").click(function(){
+
+    $("this").load;
+
   })
