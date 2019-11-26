98fb747c6931527e87573471ecf746e9a9c176a7
Jody Jones <jodybjones@yahoo.com>
2019-11-26 16:20:34 -0500

    changed the api's to the first two urls to pull data from. Earlier urls were not working.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,12 +1,14 @@
 var APIKey = "17bc6b3b7f3e498ad7423208cfc28809"
+var accukey = "	uIGrHfeAuABOGbjzAYMdcbDPwpawFiJ7"
 
-var currenturl = "api.openweathermap.org/data/2.5/weather?q={city name}" + APIKey
-var fiveday = "api.openweathermap.org/data/2.5/forecast?q={city name},{country code}" + APIKey
+var currenturl = "http://dataservice.accuweather.com/locations/v1/cities/search" + accukey
+var fiveday = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + accukey
 var uvindex = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}" + APIKey
 
 //need to make the button get the data on the click!! That goes RIGHT HERE BUDDY
 
-$("lebutton").click(function(){
+$("#lebutton").click(function()
+{
 
 $.ajax({
     url: currenturl,
@@ -14,9 +16,11 @@ $.ajax({
   }).then(function(response) {
     console.log(currenturl);
     console.log(response);
+  
 
-    $("#theArea").html("<h1>" + response.name + "Weather Details</h1>");
-    $("theTemp").html("<p>" + response.temp + "<p>");
+
+    $("#theArea").html("<h1>" + response.LocalizedName + "Weather Details</h1>");
+    $("theTemp").html("<p1>" + response.temp + "<p1>");
     $("theHumidity").html("<p2>" + response.humidity + "<p2>");
     $("theWind").html("<p3>" + response.wind + "<p3>");
     $("theUV").html("<p4>" + response.uvindex + "<p4>");  
